c244dff06c65710f4b6ee040d8be4cdafcdf6b00
Daniel Ahumada <danielahumada071@gmail.com>
2026-03-12 12:22:28 -0500

    Pruebas: agregar libro con ISBN vacio y delvover libro

--- Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/dosw/tdd/library/LibraryTest.java
@@ -19,4 +19,33 @@ public class LibraryTest {
         boolean result = library.addBook(null);
         assertFalse(result);
     }
+
+    @Test
+    void testAddBookWithEmptyIsbnReturnsFalse() {
+        Library library = new Library();
+        Book book = new Book("Clean Code", "Robert Martin", "");
+        boolean result = library.addBook(book);
+        assertFalse(result);
+    }
+
+
+    @Test
+    void testReturnLoanChangesStatusToReturned() {
+        Library library = new Library();
+    
+        User user = new User();
+        user.setId("U01");
+        user.setName("Ahumada");
+        library.addUser(user);
+    
+        Book book = new Book("Clean Code", "Robert Martin", "978-01");
+        library.addBook(book);
+    
+        Loan loan = library.loanABook("U01", "978-01");
+        Loan returned = library.returnLoan(loan);
+    
+        assertNotNull(returned);
+        assertEquals(LoanStatus.RETURNED, returned.getStatus());
+    }
+    
 }
